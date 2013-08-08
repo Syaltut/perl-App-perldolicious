306a1d90fd9756379842eb34cb4442825bf33c2f
Ahmad Syaltut <Isyaltut@gmail.com>
2013-08-08 20:24:14 +0700

    initial commit

---
+++ lib/App/perldolicious.pm
@@ -0,0 +1,20 @@
+package App::perldolicious;
+
+our $VERSION = '0.001';
+$VERSION = eval $VERSION;
+
+1;
+
+=pod
+
+=head1 NAME
+
+App::perldolicious - Mojolicious::Lite webapp that lets you search (with regexp) and display CPAN modules documentation
+
+=head1 VERSION
+
+Version 0.001.
+
+=head1 DESCRIPTION
+
+See the documentation of B<perldolicious(1)>.
